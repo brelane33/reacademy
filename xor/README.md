372756cf9d9363fb46a3b7fd11fd75a0383fef5d
Brendan Lane <brendan.lane27@gmail.com>
2018-03-24 11:10:33 -0500

    First commit to reacademy!

---
+++ xor/README.md
@@ -0,0 +1,2 @@
+# reacademy
+pdf files detailing reverse engineering for small programs
---
+++ xor/xor2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+	int a = 5;
+	int b = 6;
+
+	a = b ^ a;
+	b = a ^ b;
+	a = b ^ a;
+
+}
+
